210016fed1a7745323af61e7666354ea1a41555f
Ryan Eshleman <ryaneshleman@gmail.com>
2018-09-26 22:27:59 -0400

    extend script to provide counts of rows

--- unique_columns.py
+++ unique_columns.py
@@ -1,9 +1,10 @@
 """
-This script will read through a .csv file and gather the unique values for the provided headers.
+This script will read through a .csv file and gather the unique combinations of the provided headers.  The flag -c will
+also provide the counts of those unique rows
 
 usage:
 
-python unique_columns.py path/to/input.csv -l 'column1' 'column2' 'column3'
+python unique_columns.py path/to/input.csv -c -l 'column1' 'column2' 'column3'
 
 
 output will be written to the file output.csv
@@ -13,33 +14,40 @@ output will be written to the file output.csv
 import argparse
 import csv
 
-if __name__== "__main__":
+from collections import defaultdict
+from itertools import chain
+
+if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("input_file", type=str)
-    parser.add_argument('-l', '--list-headers', nargs='+', default=[], required=True,help= "<Required> list of quoted column headers like -l 'column1' 'column2' 'column three' ")
+    parser.add_argument("-c", '--with-counts', default=False, action='store_true')
+    parser.add_argument('-l', '--list-headers', nargs='+', default=[], required=True,
+                        help="<Required> list of quoted column headers like -l 'column1' 'column2' 'column three' ")
     args = parser.parse_args()
 
     output_file = 'output.csv'
 
     headers = args.list_headers
+    with_counts = args.with_counts
 
-    uniques = set()
+    uniques = defaultdict(int)
     with open(args.input_file) as handle:
         csv_dict = csv.DictReader(handle)
         for row in csv_dict:
             entry = tuple([row[header] for header in headers])
-            uniques.add(entry)
+            uniques[entry] += 1
 
-    output_lines = sorted(list(uniques), key=lambda x:int(x[0]) if x[0].isdigit() else x[0])
-    with open(output_file,'w') as handle:
+    if with_counts:
+        rows = [tuple(chain(key,[str(value)])) for key,value in uniques.items()]
+        output_lines= sorted(list(rows), key=lambda x: int(x[0]) if x[0].isdigit() else x[0])
+    else:
+        output_lines = sorted(list(uniques.keys()), key=lambda x: int(x[0]) if x[0].isdigit() else x[0])
 
-        handle.write(','.join(headers)+'\n')
+    with open(output_file, 'w') as handle:
+
+        handle.write(','.join(headers) + '\n')
 
         for unique in output_lines:
             out_line = ','.join(unique) + '\n'
             handle.write(out_line)
-
-
-
-
